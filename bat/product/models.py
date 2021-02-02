cb09ad90a149de6063731045a29ef77da78629b7
Chetan Singh <chetanbadgujar92@gmail.com>
2021-02-02 08:07:44 +0100

    component fixes

--- bat/product/models.py
+++ bat/product/models.py
@@ -284,7 +284,7 @@ class Product(
         blank=True,
         null=True,
         verbose_name="Select Parent",
-        related_name="product_parent",
+        related_name="parents",
     )
     create_date = models.DateTimeField(default=timezone.now)
     update_date = models.DateTimeField(default=timezone.now)
