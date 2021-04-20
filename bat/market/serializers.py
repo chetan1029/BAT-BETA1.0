fa466af7424bb78b6a15cf313eb5e41997ff4528
Chetan <chetanbadgujar92@gmail.com>
2021-04-20 10:02:32 +0200

    added Amazon Company

--- bat/market/serializers.py
+++ bat/market/serializers.py
@@ -1,15 +1,20 @@
 from rest_framework import serializers
 
-from bat.market.models import AmazonMarketplace, AmazonOrder, AmazonProduct, AmazonAccounts
-
-from bat.serializersFields.serializers_fields import CountrySerializerField
+from bat.market import constants
+from bat.market.models import (
+    AmazonAccounts,
+    AmazonCompany,
+    AmazonMarketplace,
+    AmazonOrder,
+    AmazonProduct,
+)
 from bat.product.serializers import ImageSerializer
 from bat.serializersFields.serializers_fields import (
-    TagField,
-    StatusField,
+    CountrySerializerField,
     MoneySerializerField,
+    StatusField,
+    TagField,
 )
-from bat.market import constants
 
 
 class AmazonMarketplaceSerializer(serializers.ModelSerializer):
@@ -18,13 +23,17 @@ class AmazonMarketplaceSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AmazonMarketplace
-        fields = ("id", "name", "country", "marketplaceId", "region", "status", )
+        fields = ("id", "name", "country", "marketplaceId", "region", "status")
 
     def get_status(self, obj):
         company_id = self.context.get("company_id")
         user = self.context.get("user")
         accounts = AmazonAccounts.objects.filter(
-            marketplace_id=obj.id, user_id=user.id, company_id=company_id, is_active=True)
+            marketplace_id=obj.id,
+            user_id=user.id,
+            company_id=company_id,
+            is_active=True,
+        )
         if accounts.exists():
             return constants.MARKETPLACE_STATUS_ACTIVE
         return constants.MARKETPLACE_STATUS_INACTIVE
@@ -37,12 +46,23 @@ class SingleAmazonProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AmazonProduct
-        fields = ("id", "amazonaccounts", "images", "title",
-                  "sku", "ean", "asin", "type",
-                  "url", "tags", "bullet_points",
-                  "description", "status",
-                  "extra_data", "parent",
-                  )
+        fields = (
+            "id",
+            "amazonaccounts",
+            "images",
+            "title",
+            "sku",
+            "ean",
+            "asin",
+            "type",
+            "url",
+            "tags",
+            "bullet_points",
+            "description",
+            "status",
+            "extra_data",
+            "parent",
+        )
 
 
 class AmazonProductSerializer(serializers.ModelSerializer):
@@ -53,11 +73,23 @@ class AmazonProductSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AmazonProduct
-        fields = ("id", "amazonaccounts", "images", "title",
-                  "sku", "ean", "asin", "type",
-                  "url", "tags", "bullet_points", "description",
-                  "status", "extra_data", "parent",
-                  )
+        fields = (
+            "id",
+            "amazonaccounts",
+            "images",
+            "title",
+            "sku",
+            "ean",
+            "asin",
+            "type",
+            "url",
+            "tags",
+            "bullet_points",
+            "description",
+            "status",
+            "extra_data",
+            "parent",
+        )
 
 
 class AmazonOrderSerializer(serializers.ModelSerializer):
@@ -76,13 +108,58 @@ class AmazonOrderSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = AmazonOrder
-        fields = ("id", "order_id", "order_seller_id", "purchase_date",
-                  "payment_date", "shipment_date", "reporting_date", "replacement",
-                  "status", "sales_channel", "buyer_email", "quantity",
-                  "promotion_quantity", "business_order", "amount", "tax",
-                  "shipping_price", "shipping_tax", "gift_wrap_price",
-                  "gift_wrap_tax", "item_promotional_discount",
-                  "ship_promotional_discount", "fba_fullfilment_amount",
-                  "amazon_comission_amount", "manufacturing_amount",
-                  "amazonaccounts", "extra_data",
-                  )
+        fields = (
+            "id",
+            "order_id",
+            "order_seller_id",
+            "purchase_date",
+            "payment_date",
+            "shipment_date",
+            "reporting_date",
+            "replacement",
+            "status",
+            "sales_channel",
+            "buyer_email",
+            "quantity",
+            "promotion_quantity",
+            "business_order",
+            "amount",
+            "tax",
+            "shipping_price",
+            "shipping_tax",
+            "gift_wrap_price",
+            "gift_wrap_tax",
+            "item_promotional_discount",
+            "ship_promotional_discount",
+            "fba_fullfilment_amount",
+            "amazon_comission_amount",
+            "manufacturing_amount",
+            "amazonaccounts",
+            "extra_data",
+        )
+
+
+class AmazonCompanySerializer(serializers.ModelSerializer):
+    """Amazon Company serializers."""
+
+    country = CountrySerializerField(required=False)
+
+    class Meta:
+        model = AmazonCompany
+        fields = (
+            "id",
+            "address1",
+            "address2",
+            "zip",
+            "city",
+            "region",
+            "country",
+            "store_name",
+            "name",
+            "email",
+            "phone_number",
+            "organization_number",
+            "vat_number",
+            "extra_data",
+        )
+        read_only_fields = ("id", "extra_data")
