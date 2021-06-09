cdaea8dd937e0de8f328ed2e3bbb3b2db522ba13
Chetan <chetanbadgujar92@gmail.com>
2021-06-09 18:33:22 +0200

    fixes

--- bat/autoemail/signals.py
+++ bat/autoemail/signals.py
@@ -1,21 +1,23 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+from bat.autoemail.constants import EMAIL_CAMPAIGN_STATUS_ACTIVE
 from bat.autoemail.models import EmailCampaign
-from bat.autoemail.constants import (
-    EMAIL_CAMPAIGN_STATUS_ACTIVE,
-)
 from bat.autoemail.tasks import email_queue_create_for_new_campaign
 
 
 @receiver(post_save, sender=EmailCampaign)
 def archive_component_me_on_discontinued(sender, instance, created, **kwargs):
-    if created :
+    if created:
         if instance.status.name == EMAIL_CAMPAIGN_STATUS_ACTIVE:
             email_queue_create_for_new_campaign.delay(instance.id)
-    else :
-        old_status = instance.get_dirty_fields(check_relationship=True).get("status", None)
+    else:
+        old_status = instance.get_dirty_fields(check_relationship=True).get(
+            "status", None
+        )
         if old_status:
-            if old_status != instance.status.id and instance.status.name == EMAIL_CAMPAIGN_STATUS_ACTIVE:
+            if (
+                old_status != instance.status.id
+                and instance.status.name == EMAIL_CAMPAIGN_STATUS_ACTIVE
+            ):
                 email_queue_create_for_new_campaign.delay(instance.id)
-
