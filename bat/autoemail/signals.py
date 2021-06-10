ffe688198cde5e65d9a127e59fa0897de55ce661
Chetan <chetanbadgujar92@gmail.com>
2021-06-10 09:27:24 +0200

    fixes

--- bat/autoemail/signals.py
+++ bat/autoemail/signals.py
@@ -1,3 +1,4 @@
+from celery.utils.log import get_task_logger
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -5,9 +6,12 @@ from bat.autoemail.constants import EMAIL_CAMPAIGN_STATUS_ACTIVE
 from bat.autoemail.models import EmailCampaign
 from bat.autoemail.tasks import email_queue_create_for_new_campaign
 
+logger = get_task_logger(__name__)
+
 
 @receiver(post_save, sender=EmailCampaign)
 def when_emailcampaign_created_or_updated(sender, instance, created, **kwargs):
+    logger.info("Email Campaign " + str(instance.id) + " --- " + str(created))
     if created:
         if instance.status.name == EMAIL_CAMPAIGN_STATUS_ACTIVE:
             email_queue_create_for_new_campaign.delay(instance.id)
